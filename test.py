805d9f2fcb529111222db5ff2bab2663f4e1f8ec
Yuri <khvyus@gmail.com>
2020-12-12 21:53:30 +1000

    optimize algorithm

--- test.py
+++ test.py
@@ -6,10 +6,10 @@ from skimage.color import rgb2gray
 
 
 SKIPPED_FRAMES_COUNT = 60
-LIMITS = (600, 720)
+LIMITS = (600, 750)
 THRESH_LIMIT = 10
 MAX_DEVIATION = 500
-GAP_LIMIT = 40
+GAP_LIMIT = 400 #40
 
 
 def image_processing(image):
@@ -28,10 +28,14 @@ def find_first_column(binary_image):
     start_column = binary_image.shape[1] - 1
     while binary_image[0, start_column] == 1:
         start_column -= 1
+        if start_column == 0:
+            return binary_image.shape[1] - 1
     return start_column
 
 
-def find_next_column(binary_image, row, column):
+def find_next_column(binary_image, row, column, current_border):
+    old_column = column
+
     # ищем ноль, ближайший к column, слева
     ok_left = True
     new_column_left = column
@@ -55,8 +59,9 @@ def find_next_column(binary_image, row, column):
             break
 
     if not ok_right and not ok_left:
-        pass
+        # pass
         # column = None
+        return column
     elif ok_left and not ok_right:
         column = new_column_left
     elif ok_right and not ok_left:
@@ -68,6 +73,15 @@ def find_next_column(binary_image, row, column):
             column = new_column_left
         else:
             column = new_column_right
+
+    c1 = min(old_column, column)
+    c2 = max(old_column, column)
+    old = binary_image[row-1, c1:c2+1]
+    if np.sum(old)/len(old) > 0.5:
+        if len(current_border) > 0:
+            mean = np.mean(current_border)
+            if abs(mean-column) > abs(mean-old_column):
+                column = old_column
     return column
 
 
@@ -81,19 +95,20 @@ def generate_data(file_name):
             #     counter += 1
             #     continue
 
-            gray_image, binary_image = image_processing(image)
-            start_column = find_first_column(binary_image)
-
+            border = None
             for i in range(2):
+                gray_image, binary_image = image_processing(image)
+                if border is not None:
+                    middle = int(np.mean(border))
+                    shift_border = min(middle+10, binary_image.shape[1]-1)
+                    binary_image[:, shift_border:] = 1
+                start_column = find_first_column(binary_image)
                 row = 0
-                if i == 0:
-                    column = start_column
-                else:
-                    column = min(start_column, int(np.mean(border)+5))
+                column = start_column
                 border = [column, ]
                 while row < binary_image.shape[0]-1:
                     row += 1
-                    column = find_next_column(binary_image, row, column)
+                    column = find_next_column(binary_image, row, column, border)
                     if column is not None:
                         border.append(column)
                     else:
@@ -103,30 +118,34 @@ def generate_data(file_name):
             delta_border = np.diff(border)
             min_a = np.min(delta_border)
             min_index = np.argmin(delta_border)
-
             next_part = delta_border[min_index + 1: min(min_index + 100, len(delta_border))]
             max_a = np.max(next_part)
             max_index = np.argmax(next_part) + min_index+1
-
             thresh = max_a - min_a
-            if (thresh > THRESH_LIMIT) and (max_index > min_index):
+
+            if (thresh > THRESH_LIMIT) and (abs(min_a) > THRESH_LIMIT/2) and (max_a > THRESH_LIMIT/2) and (max_index > min_index):
                 counter = 0
                 gap = max_index - min_index
 
                 if gap < GAP_LIMIT:
                     print(f"gap: {gap}")
+                    continue
 
-                    pyplot.subplot(1,3,1)
+                    pyplot.subplot(1,4,1)
                     pyplot.plot(delta_border, np.arange(binary_image.shape[0]-1))
                     pyplot.plot([min_a, max_a], [min_index, max_index], "ro")
                     pyplot.gca().invert_yaxis()
 
-                    pyplot.subplot(1,3,2)
+                    pyplot.subplot(1,4,2)
                     pyplot.imshow(binary_image)
                     pyplot.plot(border, np.arange(binary_image.shape[0]), color="red")
 
-                    pyplot.subplot(1,3,3)
+                    pyplot.subplot(1,4,3)
                     pyplot.imshow(gray_image)
+
+                    pyplot.subplot(1,4,4)
+                    pyplot.imshow(rgb2gray(image[-50: -20, 24: 170]))
+
 
                     figManager = pyplot.get_current_fig_manager()
                     figManager.window.showMaximized()
@@ -134,7 +153,7 @@ def generate_data(file_name):
                     pyplot.close()
             else:
                 counter += 1
-        except:
+        except ValueError:
             counter += 1
             print("ERROR")
     cap.release()
